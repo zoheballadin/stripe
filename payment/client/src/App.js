a0ecd95a7dcb0cfa088ea15e38ee2a3a77879018
zoheballadin <beanzoheb@gmail.com>
2023-04-21 13:43:41 +0530

    webhooks and axios

--- payment/client/src/App.js
+++ payment/client/src/App.js
@@ -1,26 +1,45 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import {useNavigate} from "react-router-dom"
 
-const ProductDisplay = () => (
-  <section>
-    <div className="product">
-      <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
-      />
-      <div className="description">
-      <h3>Stubborn Attachments</h3>
-      <h5>$20.00</h5>
+
+const ProductDisplay = () => {
+  const navigate = useNavigate()
+  const onSubmit = async(e) =>{
+    try {
+      e.preventDefault();
+      let {data} = await axios.post("/create-checkout-session")
+      console.log(data)
+      window.location = data
+      // navigate(res.request.responseURL)
+  
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  
+  return(
+    <section>
+      <div className="product">
+        <img
+          src="https://i.imgur.com/EHyR2nP.png"
+          alt="The cover of Stubborn Attachments"
+        />
+        <div className="description">
+        <h3>Stubborn Attachments</h3>
+        <h5>$20.00</h5>
+        </div>
       </div>
-    </div>
-    <form action="/create-checkout-session" method="POST">
-      <button type="submit">
-        Checkout
-      </button>
-    </form>
-  </section>
-);
-
+      <form onSubmit={onSubmit}>
+        <button type="submit">
+          Checkout
+        </button>
+      </form>
+    </section>
+  );
+}
 const Message = ({ message }) => (
   <section>
     <p>{message}</p>
--- payment/server/app.js
+++ payment/server/app.js
@@ -5,7 +5,7 @@ const stripe = require('stripe')('sk_test_51MywVjSBjoQyaRUrACnheELA6DfEnRIvdfKDR
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser")
-app.use(express.json())
+// app.use(express.json())
 app.use(express.static('public'));
 
 const YOUR_DOMAIN = 'http://localhost:4242';
@@ -24,16 +24,48 @@ app.post('/create-checkout-session', async (req, res) => {
     success_url: `http://localhost:3000`,
     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
   });
-
-  res.redirect(303, session.url);
+  
+  console.log(session.url)
+  res.json(session.url)
+//   res.redirect(303, session.url);
 });
 
-app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+const fulfillOrder = (lineItems) => {
+    // TODO: fill me in
+    console.log("Fulfilling order", lineItems);
+  }
+
+const endpointSecret = "whsec_20df996404feeab7dd9bb8a3479048e4214f5494e242f4402be4e1883fde2446";
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), async(request, response) => {
     const payload = request.body;
+    const sig = request.headers['stripe-signature'];
   
-    console.log(payload);
+    let event;
   
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    // Handle the checkout.session.completed event
+    if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+        console.log("sesh ",session)
+      // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+      const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+        session.id,
+        {
+          expand: ['line_items'],
+        }
+      );
+      const lineItems = session.line_items;
+  
+      // Fulfill the purchase...
+      fulfillOrder(sessionWithLineItems.line_items);
+    }
     response.status(200).end();
   });
+  
 
 app.listen(4242, () => console.log('Running on port 4242'));
